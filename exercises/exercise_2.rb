3384e0074192ff1c4d31f099467cf85e3017fe0c
Aisha Sabrie <sabriealt@gmail.com>
2022-02-01 17:38:51 -0500

    Write code to extract stores from db and update siad stores

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# updates the annual revenue of the store
+@store1.annual_revenue = 1000000
