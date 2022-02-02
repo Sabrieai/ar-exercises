34876dd26517001ea373f4152dd2d1cdf7a04982
Aisha Sabrie <sabriealt@gmail.com>
2022-02-01 19:30:59 -0500

    linked relations Stores and Employees together, created employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee 
+  belongs_to :store
+end
+
+#creating employees
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Celia", last_name: "Mae", hourly_rate: 80)
+@store2.employees.create(first_name: "Mike", last_name: "Wazowski", hourly_rate: 65)
+@store2.employees.create(first_name: "James", last_name: "Sullivan", hourly_rate: 70)
+@store2.employees.create(first_name: "Randall", last_name: "Boggs", hourly_rate: 55)
+
